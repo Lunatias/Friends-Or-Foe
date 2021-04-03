40cdc4e6e1da5369c0f73a1d7bd3a6bfd766565a
Sheyla Perez <sheylavperez@gmail.com>
2021-04-02 20:57:31 -0400

    Update Movement Player
    Add some code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
